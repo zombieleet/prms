f080f2900218e807cef6145170567cbfbdeeaeb3
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-07-14 10:53:39 +0100

    handle dark and white colors and also change the interval seconds

--- src/renderer/js/FakeLoading.js
+++ src/renderer/js/FakeLoading.js
@@ -1,6 +1,10 @@
 ; ( () => {
+
     "use strict";
-    console.log("hi");
+
+    const { remote: { getCurrentWindow, app } } = require("electron");
+    const emitStopInterval = new(require("events"));
+    
     document.addEventListener("DOMContentLoaded", () => {
         const title = document.querySelector("title");
         
@@ -8,18 +12,48 @@
             if ( title.textContent === "Loading" ) {
                 title.textContent = "Loading.";
             }
-        }, 500);
+        }, 500 / 2);
 
         const dotTwo = setInterval( () => {
             if ( title.textContent === "Loading." ) {
                 title.textContent = "Loading..";
             }
-        }, 1000);
+        }, 1000 / 2);
 
         const dotThree = setInterval( () => {
             if ( title.textContent === "Loading.." ) {
                 title.textContent = "Loading...";
             }
-        }, 1500);
+        }, 1500 / 2);
+
+        const dotFour = setInterval( () => {
+            if ( title.textContent === "Loading..." ) {
+                title.textContent = "Loading";
+            }
+        }, 2000 / 2);
+
+        emitStopInterval.on("prison:interval:stop", () => {
+
+            clearInterval(dotOne);
+            clearInterval(dotTwo);
+            clearInterval(dotThree);
+            clearInterval(dotFour);
+            
+            const { webContents } = getCurrentWindow();
+            webContents.clearHistory();
+            webContents.loadURL(`file://${app.getAppPath()}/src/renderer/pug/index.jade`);
+        });
+
+        
     });
+
+    setTimeout( () => {
+        emitStopInterval.emit("prison:interval:stop");
+    }, 10000);
+
+
+    if ( ! localStorage.getItem("color-mode") )
+        localStorage.setItem("color-mode", "dark");
+    else if ( localStorage.getItem("color-mode") === "white")
+        document.body.setAttribute("data-color-mode", "white");
 })();
