9e9c02e1611a4ef9cf2a7948a8652e9afd57da86
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-07-15 05:42:51 +0100

    create cell numbers collection

--- src/renderer/js/db.js
+++ src/renderer/js/db.js
@@ -1,12 +1,44 @@
 const path = require("path");
-const { remote: { app }  } = require("electron");
+const { remote: { app, dialog }  } = require("electron");
+
+
+const DataStore = require("nedb");
+
+            
+const cellCollection = new(require("nedb"))({
+    filename: path.join(app.getPath("userData"), "cell.db"),
+    autoload: true
+});
+
+cellCollection.count({}, ( err, count ) => {
+    if ( err ) {
+        dialog.showErrorBox("unexpected error", "cannot initiaize database");
+        return ;
+    }
+    if ( count !== 0 )
+        return ;
+    for ( let i = 0; i < 300 ; i++ ) {
+        cellCollection.insert( { cellNumber: i, filledUp: false, maxCellMate: 4 } , ( err, doc ) => {
+            if ( err ) {
+                dialog.showErrorBox("unexpected error", "cannot initiaize database");
+                return ;
+            }
+        });
+    }
+});
+
 
 module.exports = class DB {
     constructor(collection) {
-        this.db = new(require("nedb"))({
-            filename: path.join(app.getPath("userData"), collection),
-            autoload: true
+        
+        this.db = new DataStore({
+            filename: path.join(app.getPath("userData"), collection)
         });
+        
+        this.DataStore = DataStore;
+        this.err = err => err ? dialog.showErrorBox("unexpected error", "cannot load database") : "";
+        
+        this.db.loadDatabase( this.err );
     }
 
     add(prisonerCred) {
