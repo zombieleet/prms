124f4d0e2206e2a94871019633a285143baab3ef
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-07-15 05:42:23 +0100

    getCellMate function to find possible inmate for prisoner

--- src/renderer/js/Prisoner.js
+++ src/renderer/js/Prisoner.js
@@ -1,6 +1,8 @@
 
 const DB = require("./db.js");
-const { remote: { app, getCurrentWindow } } = require("electron");
+const path = require("path");
+
+const { remote: { app, dialog, getCurrentWindow } } = require("electron");
 const { webContents } = getCurrentWindow();
 
 // const {
@@ -15,11 +17,35 @@ const { webContents } = getCurrentWindow();
 // } = prisonerCred;
 
 module.exports = new(class Prisoner extends DB {
+    
     constructor(collection) {
         super(collection);
     }
     addPrisoner() {
         webContents.loadURL(`file://${app.getAppPath()}/src/renderer/pug/AddPrisoner.jade`);
     }
-    
+    getCellMate(crimeCommitted = undefined) {
+        
+        const cellMate = new this.DataStore({
+            filename: path.join(app.getPath("userData"), "cell")
+        });
+        
+        cellMate.loadDatabase( this.err );
+        
+        return new Promise( ( resolve, reject ) => {
+
+            cellMate.find( { filledUp: false }, ( err, { cellNumber } ) => {
+                if ( err ) {
+                    reject(err);
+                    return ;
+                }
+                this.db.find( { cellNumber, crimeCommitted: { $nin: crimeCommitted } } , ( err , doc ) => {
+                    if ( err )
+                        return reject(err);
+                    return resolve(doc);
+                });
+            });
+        });
+
+    }
 })("prisoner");
