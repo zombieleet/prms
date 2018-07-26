1748fbac463fd949360f0024d5956728e599a415
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-07-26 01:35:38 +0100

    retrieving of saved prisoner information from localStorage

--- src/renderer/js/FamilyCred.js
+++ src/renderer/js/FamilyCred.js
@@ -1,10 +1,45 @@
 ; ( () => {
+
+    const { remote: { dialog } } = require("electron");
     
     const util = require("../js/util.js");
     const bview = document.querySelector(".prev-view");
+    const save = document.querySelector(".save");
+    const form = document.querySelector("form");
 
+    const prisoner = require("../js/Prisoner.js");
 
-    bview.addEventListener("click", util.navigateWebView);
+    save.addEventListener("click", async (evt) => {
+        
+        evt.preventDefault();
+        
+        const formSections = form.querySelectorAll(".fam-info");
+        const storage = JSON.parse(localStorage.getItem("PRISONER_STORAGE"));
+
+        localStorage.removeItem("PRISONER_STORAGE");
+        
+        storage.family = [];
+        
+        Array.from(formSections, section => {
+            const buildFamily = {};
+            util.processPairs(section, (key,value) => {
+                buildFamily[key] = value;
+            });
+            storage.family.push(buildFamily);
+        });
+
+        let res;
+        
+        if ( ! ( res = await prisoner.save(storage) ) ) {
+            dialog.showErrorBox("Cannot save data", "error while saving prisoner information " + res);
+            return;
+        }
+
+        console.log("done");
+        
+    });
     
+    bview.addEventListener("click", util.navigateWebView);
     util.preventDataListDefault();
+    
 })();
