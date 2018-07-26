c446a8520502edaf6629add1d11bbab2e55b87a1
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-07-26 23:47:34 +0100

    initialize cell numbers from the main process

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, nativeImage } = require("electron");
+const { dialog, app, BrowserWindow, nativeImage } = require("electron");
 
 const electronCompile = require("electron-compile");
 const { enableLiveReload } = electronCompile;
@@ -11,6 +11,29 @@ require("electron-reload")(appRoot, {
     electron: path.join(appRoot, "node_modules", ".bin", "electron")
 });
 
+            
+const cellCollection = new(require("nedb"))({
+    filename: path.join(app.getPath("userData"), "cell.db"),
+    autoload: true
+});
+
+cellCollection.count({}, ( err, count ) => {
+    if ( err ) {
+        dialog.showErrorBox("unexpected error", "cannot initiaize database");
+        return ;
+    }
+    if ( count !== 0 )
+        return ;
+    for ( let i = 1; i < 300 ; i++ ) {
+        cellCollection.insert( { cellNumber: i, filledUp: false, maxCellMate: 4 } , ( err, doc ) => {
+            if ( err ) {
+                dialog.showErrorBox("unexpected error", "cannot initiaize database");
+                return ;
+            }
+        });
+    }
+});
+
 app.on("ready", () => {
 
     require("./menu.js")();
@@ -21,7 +44,10 @@ app.on("ready", () => {
         center: true,
         title: "Prison Management System",
         backgroundColor: "#2e2c29",
-        icon: nativeImage.createFromPath(`${appRoot}/src/main/images/logo.png`)
+        icon: nativeImage.createFromPath(`${appRoot}/src/main/images/logo.png`),
+        webPreferences: {
+            nodeIntegrationInWorker: true
+        }
     });
 
     win.loadURL(`file://${appRoot}/src/renderer/pug/loading.jade`);
