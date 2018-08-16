b54d15825463875e532a45f12259d1b351a7740c
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-08-16 03:56:39 +0100

    add support to remove warder uploaded picture whenever the reset button is clicked

--- src/renderer/js/AddWarder.js
+++ src/renderer/js/AddWarder.js
@@ -10,43 +10,49 @@
 
     const warderPicPrev = document.querySelector(".add-warder-picture-preview");
     const takePicture = warderPicPrev.querySelector("p");
-    
+
     const uploadPics = document.querySelector(".__picture__trigger");
 
     const stateInput = document.querySelector("[list=stateList]");
+    const employmentDate = document.querySelector("[name=empdate]");
+
     const util = require("../js/util.js");
 
 
     const fileReader = new FileReader();
 
 
-    const rmPicture = () => {
-        const xx = document.createElement("i");
+    const rmPicture = evt => {
+
+        const img = warderPicPrev.querySelector("img");
+        const xx = warderPicPrev.querySelector(".close-preview");
+        const p = warderPicPrev.querySelector("p");
+        const container = document.querySelector(".__picture__preview__container");
+
+        container.value = img.src = "";
+
+        img.classList.add("hide");
+        img.classList.remove("preview-warder-picture");
+
+        p.classList.remove("hide");
+
         
+        if ( xx )
+            xx.remove();
+    };
+
+    const addPicture = () => {
+        const xx = document.createElement("i");
+
         xx.classList.add("fa");
         xx.classList.add("fa-times-circle");
         xx.classList.add("close-preview");
 
-        xx.addEventListener("click", evt => {
-            
-            const img = warderPicPrev.querySelector("img");
-            const p = warderPicPrev.querySelector("p");
-            const container = document.querySelector(".__picture__preview__container");
+        xx.addEventListener("click", rmPicture);
 
-            container.value = img.src = "";
-            
-            img.classList.add("hide");
-            img.classList.remove("preview-warder-picture");
-            
-            p.classList.remove("hide");
-            
-            xx.remove();
-            
-        });
-        
         return xx;
     };
-    
+
 
     save.addEventListener("click", async (evt) => {
 
@@ -58,7 +64,7 @@
         const ADD_WARDER = {};
 
         let res;
-        
+
         Array.from(form.children, el => {
             if ( ! el.hasAttribute("data-db") )
                 return ;
@@ -68,13 +74,13 @@
         });
 
 
-        
-        
+
+
         if ( ! ( res = await warder.save(ADD_WARDER) ) ) {
             dialog.showErrorBox(`unexpected error`,`cannot save this information, contact the administrator`);
             return ;
         }
-        
+
         res.viewWarder();
     });
 
@@ -93,19 +99,32 @@
 
         const img = warderPicPrev.querySelector("img");
         const container = document.querySelector(".__picture__preview__container");
-        
+
         const { target: { result } } = evt;
-        
+
         takePicture.classList.add("hide");
 
         container.value = img.src = result;
-        
+
         img.classList.remove("hide");
         img.classList.add("preview-warder-picture");
-        
-        warderPicPrev.appendChild(rmPicture());
-        
+
+        warderPicPrev.appendChild(addPicture());
+
     });
+
+
+    employmentDate.addEventListener("change", evt => {
+        const retirementDateEl = document.querySelector("[name=rtrdate]");
+        const date = evt.target.valueAsDate;
+        const retirementYear = date.getFullYear() + 32;
+
+        retirementDateEl.valueAsDate = new Date(`${date.getMonth() + 1}/${date.getDate()}/${retirementYear}`);
+
+    });
+
+
+    form.addEventListener("reset", rmPicture);
 
     stateInput.addEventListener("input", util.lgaOfState);
     util.preventDataListDefault();
