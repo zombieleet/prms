596eb799ee8daeb3530a1c3c27e9ebb1dd7ee25f
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-08-21 03:04:06 +0100

    add viewWarder and deleteWarder method

--- src/renderer/js/Warder.js
+++ src/renderer/js/Warder.js
@@ -3,13 +3,20 @@ const path = require("path");
 const { remote: { getCurrentWindow, app } } = require("electron");
 const { webContents } = getCurrentWindow();
 
-module.exports = new(class Warder  extends DB {
+module.exports = new(class Warder extends DB {
+    
     constructor(collection) {
         super(collection);
     }
+    
     addWarder() {
         webContents.loadURL(`file://${app.getAppPath()}/src/renderer/pug/AddWarder.jade`);
     }
+    
+    viewWarder() {
+        webContents.loadURL(`file://${app.getAppPath()}/src/renderer/pug/ViewWarders.jade`);
+    }
+    
     async save(data) {
         let result;
         try {
@@ -22,7 +29,26 @@ module.exports = new(class Warder  extends DB {
         }
         return this;
     }
-    viewWarder() {
-        webContents.loadURL(`file://${app.getAppPath()}/src/renderer/pug/ViewWarders.jade`);
+    async __viewWarder( query = {} ) {
+        let result ;
+        try {
+            result = await  this.view(query);
+        } catch(ex) {
+            result = ex;
+        }
+        if ( Error[Symbol.hasInstance](result) )
+            return false;
+        return result;
+    }
+    async deleteWarder(query = {}, options = {} ) {
+        let result;
+        try {
+            result = await this.delete(query, options);
+        } catch(ex) {
+            result = ex;
+        }
+        if ( Error[Symbol.hasInstance](result) )
+            return false;
+        return true;
     }
 })("warder.db");
