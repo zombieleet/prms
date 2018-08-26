edf242ce07ef1f8da036e224e3e4e00cbecf355d
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-08-26 08:20:53 +0100

    fix bug that does not change the look of the icon to switch between light and dark mode if a new page is navigated into

--- src/renderer/js/HeaderButton.js
+++ src/renderer/js/HeaderButton.js
@@ -11,6 +11,24 @@
 
     const isHistory = el => el.getAttribute("data-no-history");
 
+
+    const changeMode = () => {
+        
+        if ( localStorage.getItem("color-mode") === "dark" ) {
+            changeColor.classList.remove("fa-toggle-on");
+            changeColor.classList.add("fa-toggle-off");
+            document.body.setAttribute("data-color-mode", "white");
+            return localStorage.setItem("color-mode", "white");
+        }
+
+        document.body.removeAttribute("data-color-mode");
+
+        changeColor.classList.remove("fa-toggle-off");
+        changeColor.classList.add("fa-toggle-on");
+
+        return localStorage.setItem("color-mode","dark");
+    };
+
     document.addEventListener("DOMContentLoaded", () => {
 
         const { webContents } = getCurrentWindow();
@@ -25,23 +43,11 @@
         else
             goFront.setAttribute("data-no-history","no");
 
+        changeMode();
+
     });
 
-    changeColor.addEventListener("click", () => {
-        if ( localStorage.getItem("color-mode") === "dark" ) {
-            changeColor.classList.remove("fa-toggle-on");
-            changeColor.classList.add("fa-toggle-off");
-            document.body.setAttribute("data-color-mode", "white");
-            return localStorage.setItem("color-mode", "white");
-        }
-
-        document.body.removeAttribute("data-color-mode");
-
-        changeColor.classList.remove("fa-toggle-off");
-        changeColor.classList.add("fa-toggle-on");
-
-        return localStorage.setItem("color-mode","dark");
-    });
+    changeColor.addEventListener("click", changeMode);
 
     goBack.addEventListener("click", () => isHistory(goBack) === "no" ? webContents.goBack() : "" );
     goFront.addEventListener("click", () => isHistory(goFront) === "no" ? webContents.goForward() : "");
