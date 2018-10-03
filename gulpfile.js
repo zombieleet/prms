ee71049b7270a9c62ab40e6583e4af40829533bb
zombieleet <zombieleetnca@gmail.com>
2018-10-03 09:59:03 -0700

    gulpfile to watch for changes

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const gulpPug = require("gulp-pug");
+
+gulp.task("sass", () => {
+	return gulp.src("./src/renderer/scss/main.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(gulp.dest("./src/renderer/css/"));
+});
+
+gulp.task("pug", () => {
+	return gulp.src("./src/renderer/pug/*.pug")
+		.pipe(gulpPug({}))
+		.pipe(gulp.dest("./src/renderer/pug/"));
+});
+
+
+gulp.task("watch", () => {
+	gulp.watch("./src/renderer/scss/*.scss", [ "sass" ]);
+	gulp.watch("./src/renderer/pug/*.pug", [ "pug" ]);
+});
+
+
+gulp.task("default", [ "sass", "pug", "watch" ]);
